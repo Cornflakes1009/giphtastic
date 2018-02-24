a688346406474dd13c5aa84792ca13c83a9759f5
Harold Davidson <harold.davidson89@gmail.com>
2018-02-23 23:33:54 -0600

    done for the night

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,22 +1,31 @@
 // api key t0ciuMUzF8UO9InX1mShBj5y9JfhJqOn
 $(document).ready(function () {
     
-    var gifArray = ['mangoes', 'bananas', 'kiwi', 'pineapple', 'strawberries', 'oranges'];
+    var gifArray = ['Donkey Kong', 'Zelda', 'Kirby', 'Pokemon', 'Metroid', 'Star Fox'];
+    console.log(gifArray);
 
-    for(var i = 0; i < gifArray.length; i++){
-        $('.button-area').append('<button>' + gifArray[i] + '</button>');
-        $('button').attr('class', 'btn btn-info mr-3 mt-4');
+    function addButtons(){
+        for(var i = 0; i < gifArray.length; i++){
+            $('.button-area').append("<button class='btn btn-info mr-3 mt-4 gif-button'>" + gifArray[i] + '</button>');
+            //$('button').attr('class', 'btn btn-info mr-3 mt-4');
+        };
     }
-
-    $('.submit').on('click', function () {
+    addButtons();
+    $('#submit').on('click', function () {
         var wordInSearchBox = $('.search-box').val().trim();
         if (wordInSearchBox.length > 0) {
-            //gifArray.push($('.search-box').val()); <- removed from conditional statement. Considered checking if the word was already in the array. 
-            // && gifArray.indexOf(wordInSearchBox)
             gifArray.push(wordInSearchBox);
             console.log(gifArray);
-        }
-    })
+            addButtons();
+            //$('.button-area').append("<button class='btn btn-info mr-3 mt-4 gif-button'>" + wordInSearchBox + '</button>');
+            // $('button').attr('class', 'btn btn-info mr-3 mt-4');
+            $('button').addClass('gif-button');
+        } 
+    });
+    $('.gif-button').on('click', function(){
+        alert('clicked');
+    });
+
 
 
 
